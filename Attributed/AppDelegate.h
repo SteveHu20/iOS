56c816c7380cb480ccc51c0786713721e4d482f1
baochenhu <baochenhu@gmail.com>
2015-01-21 15:25:52 -0500

    Initial Commit

---
+++ Attributed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Attributed
+//
+//  Created by Baochen Hu on 1/21/15.
+//  Copyright (c) 2015 baochen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Attributed/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Attributed
+//
+//  Created by Baochen Hu on 1/21/15.
+//  Copyright (c) 2015 baochen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
